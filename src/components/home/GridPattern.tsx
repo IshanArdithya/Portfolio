7b62a93f31a084bcb8d09157b1dd4214ef4eb567
Ishan Ardithya Bandarigoda <55695411+IshanArdithya@users.noreply.github.com>
2025-05-10 20:10:35 +0530

    Update GridPattern.tsx

--- src/components/home/GridPattern.tsx
+++ src/components/home/GridPattern.tsx
@@ -14,3 +14,17 @@ export const AnimatedCircleGridPattern = () => (
     )}
   />
 );
+
+export const AnimatedCircleFooterGridPattern = () => (
+  <AnimatedGridPattern
+    width={75}
+    height={75}
+    maxOpacity={0.3}
+    duration={0.9}
+    strokeDasharray={0}
+    className={cn(
+      "[mask-image:radial-gradient(400px_circle_at_center,white,transparent)]",
+      "inset-x-0 inset-y-[-15%] h-[200%] skew-y-0 skew-x-0"
+    )}
+  />
+);
