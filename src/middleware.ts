0b6b1bf5e190820a91224b7b0700dc7697eb40c8
Ishan Ardithya Bandarigoda <55695411+IshanArdithya@users.noreply.github.com>
2025-09-20 18:05:32 +0530

    Create middleware.ts

---
+++ src/middleware.ts
@@ -0,0 +1,15 @@
+import { NextRequest, NextResponse } from "next/server";
+
+export function middleware(request: NextRequest) {
+  const pathname = request.nextUrl.pathname;
+
+  if (pathname !== "/") {
+    return NextResponse.redirect(new URL("/", request.url));
+  }
+
+  return NextResponse.next();
+}
+
+export const config = {
+  matcher: ["/((?!api|_next/static|_next/image|favicon.ico|.*\\.).*)"],
+};
